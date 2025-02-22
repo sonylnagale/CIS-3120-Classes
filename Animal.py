e984363dcc7688ef464d2c824ca0f0c1525dec2b
Isasil440 <isabel.silva@baruchmail.cuny.edu>
2025-02-21 22:05:49 -0500

    Updated Animal and Usage

--- Animal.py
+++ Animal.py
@@ -3,6 +3,22 @@ class Animal:
         self.__name = name 
         print("hello, I am", self.__name)
 
+    #add 5 more functions
     def talk(self):
-        print("hi")
+        print("I am a parrot")
+
+    def feed(self):
+        print("Animal has been feed")
+
+    def sleep(self):
+        print("Animal is sleeping")
+
+    def roaming(self):
+        print("Animal is roaming")
+
+    def alert(self):
+        print("Animal is alert")
+    def play(self):
+        print("Animal is playing")
+    
 
